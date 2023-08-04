71862dff0a9f8b4a8b0f263b4b9eaa5ba6ea1485
Lior Banai <liorbanai@gmail.com>
2023-08-04 16:30:09 +0300

    Add Exception into the message

---
+++ Analogy.LogViewer.ElasticCommonSchema/Parsers/EcsDocumentUtils.cs
@@ -0,0 +1,46 @@
+﻿using Analogy.Interfaces;
+using Elastic.CommonSchema;
+using System;
+
+namespace Analogy.LogViewer.ElasticCommonSchema.Parsers
+{
+    public static class EcsDocumentUtils
+    {
+        public static IAnalogyLogMessage ParseLine(string line)
+        {
+            string GetMessage(EcsDocument ecsDocument)
+            {
+                if (ecsDocument.Error is not null)
+                {
+                    return
+                        $"{ecsDocument.Message}.{Environment.NewLine}Exception:{ecsDocument.Error.Message}{Environment.NewLine}StackTrace:{ecsDocument.Error.StackTrace}";
+                }
+                return ecsDocument.Message ?? "";
+            }
+            var entry = EcsDocument.Deserialize(line);
+            AnalogyLogMessage message = new()
+            {
+                Date = entry.Timestamp?.DateTime ?? DateTime.MinValue,
+                Level = AnalogyLogMessage.ParseLogLevelFromString(entry.Log.Level),
+                RawText = line,
+                RawTextType = AnalogyRowTextType.JSON,
+                Text = GetMessage(entry),
+                MachineName = entry.Host?.Hostname ?? "",
+                ProcessId = (int)(entry.Process?.Pid ?? 0),
+                LineNumber = (int)(entry.Log?.OriginFileLine ?? 0),
+                MethodName = entry.Log?.OriginFunction ?? "",
+                FileName = entry.Log?.OriginFileName ?? "",
+                User = entry.User?.Name ?? "Unknown",
+            };
+            if (entry.Error?.Message != null)
+            {
+                message.AddOrReplaceAdditionalProperty("Error",entry.Error.Message);
+            }
+            if (entry.Error?.StackTrace != null)
+            {
+                message.AddOrReplaceAdditionalProperty("StackTrace", entry.Error.StackTrace);
+            }
+            return message;
+        }
+    }
+}
--- Analogy.LogViewer.ElasticCommonSchema/Parsers/EcsJsonFileParser.cs
+++ Analogy.LogViewer.ElasticCommonSchema/Parsers/EcsJsonFileParser.cs
@@ -24,19 +24,21 @@ namespace Analogy.LogViewer.ElasticCommonSchema.Parsers
                 DateTimeZoneHandling = DateTimeZoneHandling.Utc,
             };
         }
-        public async Task<IEnumerable<IAnalogyLogMessage>> Process(string fileName, CancellationToken token, ILogMessageCreatedHandler messagesHandler)
+
+        public async Task<IEnumerable<IAnalogyLogMessage>> Process(string fileName, CancellationToken token,
+            ILogMessageCreatedHandler messagesHandler)
         {
             if (string.IsNullOrEmpty(fileName))
             {
                 AnalogyLogMessage empty = new AnalogyLogMessage($"File is null or empty. Aborting.",
                     AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
                 {
-                    Source = "Analogy",
-                    Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
+                    Source = "Analogy", Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
                 };
                 messagesHandler.AppendMessage(empty, fileName);
                 return new List<IAnalogyLogMessage> { empty };
             }
+
             List<IAnalogyLogMessage> messages = new List<IAnalogyLogMessage>();
             try
             {
@@ -47,44 +49,23 @@ namespace Analogy.LogViewer.ElasticCommonSchema.Parsers
 #endif
                 foreach (var line in jsonLines)
                 {
-                    var message = Parseline(line);
+                    var message = EcsDocumentUtils.ParseLine(line);
                     messages.Add(message);
                     messagesHandler.AppendMessage(message, fileName);
-
                 }
                 return messages;
             }
             catch (Exception e)
             {
-                AnalogyLogMessage empty = new AnalogyLogMessage($"Error occurred processing file {fileName}. Reason: {e.Message}",
+                AnalogyLogMessage empty = new AnalogyLogMessage(
+                    $"Error occurred processing file {fileName}. Reason: {e.Message}",
                     AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
                 {
-                    Source = "Analogy",
-                    Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
+                    Source = "Analogy", Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
                 };
                 messagesHandler.AppendMessage(empty, fileName);
-                return new List<AnalogyLogMessage> { empty
-    };
+                return new List<AnalogyLogMessage> { empty };
             }
-        }
-
-        public static IAnalogyLogMessage Parseline(string line)
-        {
-            var entry = EcsDocument.Deserialize(line);
-            AnalogyLogMessage message = new AnalogyLogMessage()
-            {
-                Date = entry.Timestamp?.DateTime ?? DateTime.MinValue,
-                Level = AnalogyLogMessage.ParseLogLevelFromString(entry.Log.Level),
-                RawText = line,
-                RawTextType = AnalogyRowTextType.JSON,
-                Text = entry.Message ?? "",
-                MachineName = entry.Host?.Hostname ?? "",
-                ProcessId = (int)(entry.Process?.Pid ?? 0),
-                LineNumber = (int)(entry.Log?.OriginFileLine ?? 0),
-                MethodName = entry.Log?.OriginFunction ?? "",
-                FileName = entry.Log?.OriginFileName ?? "",
-            };
-            return message;
         }
     }
 }
